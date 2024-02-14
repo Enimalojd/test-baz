28c35eb879c09996d8387543cf972fe4b2732e4f
enimalojd <enderston8@gmail.com>
2024-02-14 14:47:21 +0400

    get data logic

---
+++ gendiff/data/get_data.py
@@ -0,0 +1,33 @@
+import requests
+import json
+import os
+from ...config import DATA_PATH1, DATA_PATH2
+
+
+def main_dir():
+    return os.path.dirname(os.path.abspath(__file__))
+
+
+def clear_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'w') as file:
+            file.truncate(0)
+
+
+def get_sisyphus_data():
+    data = requests.get(DATA_PATH1).json()
+    res_path = main_dir() + '/json/sisyphus.json'
+    with open(res_path, 'w') as file:
+        clear_file(res_path)
+        names = {package["name"]: package["version"] for package in data['packages']}
+        json.dump(names, file)
+    return None
+
+def get_p10_data():
+    data = requests.get(DATA_PATH2).json()
+    res_path = main_dir() + '/json/p10.json'
+    with open(res_path, 'w') as file:
+        clear_file(res_path)
+        names = {package["name"]: package["version"] for package in data['packages']}
+        json.dump(names, file)
+    return None
