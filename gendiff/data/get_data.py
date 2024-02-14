cfbf3bafa7f1b0f2ab73c70c42697a74f8c43f14
enimalojd <enderston8@gmail.com>
2024-02-14 18:09:05 +0400

    get data from API

--- gendiff/data/get_data.py
+++ gendiff/data/get_data.py
@@ -1,7 +1,7 @@
+import os.path
 import requests
 import json
-import os
-from ...config import DATA_PATH1, DATA_PATH2
+from gendiff.config import DATA_PATH1, DATA_PATH2
 
 
 def main_dir():
@@ -16,25 +16,33 @@ def clear_file(file_path):
 
 def get_sisyphus_data():
     data = requests.get(DATA_PATH1).json()
-    res_path = main_dir() + '/json/sisyphus.json'
+    dir = main_dir()
+    res_path = os.path.join(dir, 'json/sisyphus.json')
     with open(res_path, 'w') as file:
         clear_file(res_path)
-        names = {package["name"]: package["version"] for package in data['packages']}
+        names = {package["name"]: package["version"]
+                 for package in data['packages']}
         json.dump(names, file)
-    return None
+    return res_path
+
 
 def get_p10_data():
     data = requests.get(DATA_PATH2).json()
-    res_path = main_dir() + '/json/p10.json'
+    dir = main_dir()
+    res_path = os.path.join(dir, 'json/p10.json')
     with open(res_path, 'w') as file:
         clear_file(res_path)
-        names = {package["name"]: package["version"] for package in data['packages']}
+        names = {package["name"]: package["version"]
+                 for package in data['packages']}
         json.dump(names, file)
-    return None
+    return res_path
 
 
 def save_data(data):
-    res_path = main_dir() + '/json/result.json'
+    dir = main_dir()
+    res_path = os.path.join(dir, 'json/result.json')
+    clear_file(res_path)
     with open(res_path, 'w') as file:
         file.write(data)
+    print(res_path)
     return None
