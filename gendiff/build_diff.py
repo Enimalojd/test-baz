5352f7afcd57d2efccf894b605ef3cdff5783476
enimalojd <enderston8@gmail.com>
2024-02-15 14:10:20 +0400

    change naming

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -2,19 +2,19 @@ def build_diff(data1, data2):
     diff = {}
     keys = sorted(set(data1.keys()) | set(data2.keys()))
     for key in keys:
-        status = {'key': key}
+        status = {'name': key}
         if key not in data2:
             status['operation'] = 'only sisyphus'
-            status['value'] = data1[key]
+            status['version'] = data1[key]
         elif key not in data1:
             status['operation'] = 'only p10'
-            status['value'] = data2[key]
+            status['version'] = data2[key]
         elif data1[key] != data2[key]:
             status['operation'] = 'different versions'
             status['old'] = data1[key]
             status['new'] = data2[key]
         else:
             status['operation'] = 'unchanged'
-            status['value'] = data2[key]
+            status['version'] = data2[key]
         diff[key] = status
     return diff
