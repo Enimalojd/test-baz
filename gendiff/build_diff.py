6616c4d1649995ff057cd61bcf967bfad4d05abe
enimalojd <enderston8@gmail.com>
2024-02-15 15:11:37 +0400

    naming + list of sisyphus packages

--- gendiff/build_diff.py
+++ gendiff/build_diff.py
@@ -1,20 +1,27 @@
+from gendiff.compare import compare_versions
+
+
 def build_diff(data1, data2):
     diff = {}
+    sisyphus_packages = []
     keys = sorted(set(data1.keys()) | set(data2.keys()))
     for key in keys:
         status = {'name': key}
         if key not in data2:
-            status['operation'] = 'only sisyphus'
+            status['branch'] = 'sisyphus'
             status['version'] = data1[key]
         elif key not in data1:
-            status['operation'] = 'only p10'
+            status['branch'] = 'p10'
             status['version'] = data2[key]
         elif data1[key] != data2[key]:
-            status['operation'] = 'different versions'
-            status['old'] = data1[key]
-            status['new'] = data2[key]
+            status['branch'] = 'different versions'
+            status['sisyphus'] = data1[key]
+            status['p10'] = data2[key]
+            if compare_versions(data1[key], data2[key]):
+                sisyphus_packages.append(key)
         else:
-            status['operation'] = 'unchanged'
+            status['branch'] = 'coincidence'
             status['version'] = data2[key]
         diff[key] = status
+    diff['sisyphus_over'] = sisyphus_packages
     return diff
